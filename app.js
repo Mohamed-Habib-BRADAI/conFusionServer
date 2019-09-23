729c326b85706b51f9635c2be5132d3502a11f9c
Bradai Mohamed Habib <hbibbradai3@gmail.com>
2019-09-24 00:00:40 +0100

    Cookies

--- app.js
+++ app.js
@@ -29,12 +29,16 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth (req, res, next)
 {
   //console.log(req.headers);
-  var authHeader = req.headers.authorization
+  if (!req.signedCookies.user)
+  {
+    var authHeader = req.headers.authorization
+  
+  
   if ( !authHeader)
   {
     var err = new Error('You are not authenticated!');
@@ -43,22 +47,34 @@ function auth (req, res, next)
     next(err);
     return ;
   }
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
 var user = auth[0] ;
 var pass = auth[1];
 if (user == 'admin' && pass =='password')
 {
-  next() ;
+  res.cookie('user','admin',{signed: true}); 
+  next();
 }
 else
 {
   var err = new Error('You are not authenticated!');
-  res.setHeader('WWW-authenticate', 'Basic');
   err.status = 401 ;
   next(err);
 }
 }
-
+else 
+{
+  if (req.signedCookies.user=== 'admin')
+  {
+    next();
+  }
+  else{
+    var err = new Error('You are not authenticated!');
+          err.status = 401;
+          next(err);
+  }
+}
+}
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
