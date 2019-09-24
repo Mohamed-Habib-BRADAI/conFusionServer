b87a12978731ddcbe47e5f76426cfbf09e249c28
Bradai Mohamed Habib <hbibbradai3@gmail.com>
2019-09-24 23:29:05 +0100

    Express Session Part 2

--- app.js
+++ app.js
@@ -39,43 +39,24 @@ app.use(session({
   resave :false ,
   store : new FileStore()
 }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 function auth (req, res, next)
 {
   console.log(req.session)
   //console.log(req.headers);
   if (!req.session.user)
   {
-    var authHeader = req.headers.authorization
-  
-  
-  if ( !authHeader)
-  {
     var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-authenticate', 'Basic');
-    err.status = 401 ;
-    next(err);
-    return ;
-  }
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-var user = auth[0] ;
-var pass = auth[1];
-if (user == 'admin' && pass =='password')
-{
-  req.session.user='admin'; 
-  next();
-}
-else
-{
-  var err = new Error('You are not authenticated!');
-  err.status = 401 ;
-  next(err);
-}
+      err.status = 403;
+      return next(err);
+  
 }
 else 
 {
-  if (req.session.user=== 'admin')
+  if (req.session.user=== 'authenticated')
   {
-    console.log('req.session: ',req.session);
     next();
   }
   else{
@@ -91,8 +72,6 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
